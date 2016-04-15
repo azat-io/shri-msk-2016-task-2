27a668b1525b0a00afc5e40ca3404bda297e136f
Azat S <to@azat.io>
2016-04-15 15:03:29 +0300

    Fix Main route component

--- src/js/App.js
+++ src/js/App.js
@@ -4,7 +4,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import Header from './Header/Header'
-import Main from './Main'
+import Main from './Main/Main'
 import Footer from './Footer'
 
 class App extends React.Component {
