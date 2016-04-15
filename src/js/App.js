1e0ff42b160632660931820f976d4ea250c817fc
Azat S <to@azat.io>
2016-04-15 14:42:22 +0300

    Add header

--- src/js/App.js
+++ src/js/App.js
@@ -3,7 +3,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Header from './Header'
+import Header from './Header/Header'
 import Main from './Main'
 import Footer from './Footer'
 
---
+++ src/js/Header/Header.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import React from 'react'
+
+class Header extends React.Component {
+  render() {
+    return <header></header>
+  }
+}
+
+export default Header
